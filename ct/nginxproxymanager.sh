3ac367ff59f25d2340e4eccb6f5173ad0667755c
Bruno Alves <57677900+balves42@users.noreply.github.com>
2025-02-27 15:41:01 +0000

    Update nginxproxymanager.sh

--- ct/nginxproxymanager.sh
+++ ct/nginxproxymanager.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/asylumexp/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/balves42/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
-# License: MIT | https://github.com/asylumexp/Proxmox/raw/main/LICENSE
+# License: MIT | https://github.com/balves42/Proxmox/raw/main/LICENSE
 # Source: https://nginxproxymanager.com/
 
 APP="Nginx Proxy Manager"
