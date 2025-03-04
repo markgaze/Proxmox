a6a551aafb083e92a940cd0152344604961dd7c5
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2025-03-04 17:47:53 +1000

    Update nginxproxymanager-install.sh

--- install/nginxproxymanager-install.sh
+++ install/nginxproxymanager-install.sh
@@ -49,15 +49,9 @@ msg_ok "Installed Python Dependencies"
 VERSION="$(awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release)"
 
 msg_info "Installing Openresty"
-#wget -qO - https://openresty.org/package/pubkey.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/openresty-archive-keyring.gpg
-#echo -e "deb http://openresty.org/package/debian bullseye openresty" >/etc/apt/sources.list.d/openresty.list
 wget -O - https://openresty.org/package/pubkey.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/openresty.gpg
-
 codename=`grep -Po 'VERSION="[0-9]+ \(\K[^)]+' /etc/os-release`
-
-echo "deb http://openresty.org/package/arm64/debian $codename openresty" \
-    | sudo tee /etc/apt/sources.list.d/openresty.list
-
+echo "deb http://openresty.org/package/arm64/debian $codename openresty" | sudo tee /etc/apt/sources.list.d/openresty.list
 $STD apt-get update
 $STD apt-get -y install openresty
 msg_ok "Installed Openresty"
